8648eb7937ac759b78651f1e9c144a4f7e1bb462
Pengcheng Yin <pcyin@cs.cmu.edu>
2018-09-17 21:53:43 -0700

    added more files

--- nmt.py
+++ nmt.py
@@ -54,7 +54,7 @@ from vocab import Vocab, VocabEntry
 Hypothesis = namedtuple('Hypothesis', ['value', 'score'])
 
 
-class NMT(nn.Module):
+class NMT(object):
 
     def __init__(self, embed_size, hidden_size, vocab, dropout_rate=0.2):
         super(NMT, self).__init__()
@@ -66,33 +66,54 @@ class NMT(nn.Module):
 
         # initialize neural network layers...
 
-    def forward(self, src_sents: List[List[str]], tgt_sents: List[List[str]]):
+    def __call__(self, src_sents: List[List[str]], tgt_sents: List[List[str]]) -> Tensor:
         """
-        take a mini-batch of source and target sentences, compute the negative log-likelihood of 
+        take a mini-batch of source and target sentences, compute the log-likelihood of 
         target sentences.
 
-        :param src_sents: list of source sentence tokens
-        :param tgt_sents: list of target sentence tokens, wrapped by `<s>` and `</s>`
+        Args:
+            src_sents: list of source sentence tokens
+            tgt_sents: list of target sentence tokens, wrapped by `<s>` and `</s>`
+
+        Returns:
+            scores: a variable/tensor of shape (batch_size, ) representing the 
+                log-likelihood of generating the gold-standard target sentence for 
+                each example in the input batch
         """
         src_encodings, decoder_init_state = self.encode(src_sents, tgt_sents)
         scores = self.decode(src_encodings, decoder_init_state, tgt_sents)
 
         return scores
 
-    def encode(self, src_sents: List[List[str]]) -> Tuple[Any, Any]:
+    def encode(self, src_sents: List[List[str]]) -> Tuple[Tensor, Any]:
         """
         Use a GRU/LSTM to encode source sentences into hidden states
 
-        :param src_encodings: hidden states of tokens in source sentences
-        :param decoder_init_state: decoder GRU/LSTM's initial state, computed from source encodings
+        Args:
+            src_sents: list of source sentence tokens
+
+        Returns:
+            src_encodings: hidden states of tokens in source sentences, this could be a variable 
+                with shape (batch_size, source_sentence_length, encoding_dim), or in orther formats
+            decoder_init_state: decoder GRU/LSTM's initial state, computed from source encodings
         """
 
         return src_encodings, decoder_init_state
 
-    def decode(self, src_encodings: Any, decoder_init_state: Any, tgt_sents: List[List[str]]) -> Any:
+    def decode(self, src_encodings: Tensor, decoder_init_state: Any, tgt_sents: List[List[str]]) -> Tensor:
         """
         Given source encodings, compute the log-likelihood of predicting the gold-standard target
         sentence tokens
+
+        Args:
+            src_encodings: hidden states of tokens in source sentences
+            decoder_init_state: decoder GRU/LSTM's initial state
+            tgt_sents: list of gold-standard target sentences, wrapped by `<s>` and `</s>`
+
+        Returns:
+            scores: could be a variable of shape (batch_size, ) representing the 
+                log-likelihood of generating the gold-standard target sentence for 
+                each example in the input batch
         """
 
         return scores
@@ -100,14 +121,57 @@ class NMT(nn.Module):
     def beam_search(self, src_sent: List[str], beam_size: int=5, max_decoding_time_step: int=70) -> List[Hypothesis]:
         """
         Given a single source sentence, perform beam search
+
+        Args:
+            src_sent: a single tokenized source sentence
+            beam_size: beam size
+            max_decoding_time_step: maximum number of time steps to unroll the decoding RNN
+
+        Returns:
+            hypotheses: a list of hypothesis, each hypothesis has two fields:
+                value: List[str]: the decoded target sentence, represented as a list of words
+                score: float: the log-likelihood of the target sentence
         """
 
         return hypotheses
+
+    def evaluate_ppl(self, dev_data: List[Any], batch_size: int=32):
+        """
+        Evaluate perplexity on dev sentences
+
+        Args:
+            dev_data: a list of dev sentences
+            batch_size: batch size
+        
+        Returns:
+            ppl: the perplexity on dev sentences
+        """
+
+        cum_loss = 0.
+        cum_tgt_words = 0.
+
+        # you may want to wrap the following code using a context manager provided
+        # by the NN library to signal the backend to not to keep gradient information
+        # e.g., `torch.no_grad()`
+
+        for src_sents, tgt_sents in batch_iter(dev_data, batch_size):
+            loss = -model(src_sents, tgt_sents).sum()
+
+            cum_loss += loss
+            tgt_word_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting the leading `<s>`
+            cum_tgt_words += tgt_word_num_to_predict
+
+        ppl = np.exp(cum_loss / cum_tgt_words)
+
+        return ppl
 
     @staticmethod
     def load(model_path: str):
         """
         Load a pre-trained model
+
+        Returns:
+            model: the loaded model
         """
 
         return model
@@ -117,18 +181,19 @@ class NMT(nn.Module):
         Save current model to file
         """
 
-
-def evaluate_ppl(model, dev_data, batch_size=32):
-    """
-    Evaluate perplexity on dev sentences
-    """
-
-    return ppl
+        raise NotImplementedError()
 
 
 def compute_corpus_level_bleu_score(references: List[List[str]], hypotheses: List[Hypothesis]) -> float:
     """
     Given decoding results and reference sentences, compute corpus-level BLEU score
+
+    Args:
+        references: a list of gold-standard reference target sentences
+        hypotheses: a list of hypotheses, one for each reference
+
+    Returns:
+        bleu_score: corpus-level BLEU score
     """
     if references[0][0] == '<s>':
         references = [ref[1:-1] for ref in references]
@@ -201,7 +266,12 @@ def train(args: Dict[str, str]):
                 train_time = time.time()
                 report_loss = report_tgt_words = report_examples = 0.
 
-            # perform validation
+            # the following code performs validation on dev set, and controls the learning schedule
+            # if the dev score is better than the last check point, then the current model is saved.
+            # otherwise, we allow for that performance degeneration for up to `--patience` times;
+            # if the dev score does not increase after `--patience` iterations, we reload the previously
+            # saved best model (and the state of the optimizer), halve the learning rate and continue
+            # training. This repeats for up to `--max-num-trial` times.
             if train_iter % valid_niter == 0:
                 print('epoch %d, iter %d, cum. loss %.2f, cum. ppl %.2f cum. examples %d' % (epoch, train_iter,
                                                                                          cum_loss / cumulative_examples,
@@ -214,7 +284,7 @@ def train(args: Dict[str, str]):
                 print('begin validation ...', file=sys.stderr)
 
                 # compute dev. ppl and bleu
-                dev_ppl = evaluate_ppl(model, dev_data, batch_size=128)   # dev batch size can be a bit larger
+                dev_ppl = model.evaluate_ppl(dev_data, batch_size=128)   # dev batch size can be a bit larger
                 valid_metric = -dev_ppl
 
                 print('validation: iter %d, dev. ppl %f' % (train_iter, dev_ppl), file=sys.stderr)
@@ -270,6 +340,11 @@ def beam_search(model: NMT, test_data_src: List[List[str]], beam_size: int, max_
 
 
 def decode(args: Dict[str, str]):
+    """
+    performs decoding on a test set, and save the best-scoring decoding results. 
+    If the target gold-standard sentences are given, the function also computes
+    corpus-level BLEU score.
+    """
     test_data_src = read_corpus(args['TEST_SOURCE_FILE'], source='src')
     if args['TEST_TARGET_FILE']:
         test_data_tgt = read_corpus(args['TEST_TARGET_FILE'], source='tgt')
