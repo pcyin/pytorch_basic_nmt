059bcc219bd3a5912142e57e8afd681dd09bc1e3
Pengcheng Yin <pcyin@cs.cmu.edu>
2018-10-02 22:18:46 -0400

    fix sample bug

--- raml_utils.py
+++ raml_utils.py
@@ -14,7 +14,7 @@ def get_reward(ref_sent, hyp_sent, smooth_func=_smooth_func):
 
 
 def mcmc_accept(p_y_tm1, q_y_tm1, p_y_t, q_y_t, tau=1.0):
-    acc_ratio = q_y_t / tau - q_y_tm1 / tau + p_y_tm1 - p_y_t
+    acc_ratio = math.exp(q_y_t / tau - q_y_tm1 / tau + p_y_tm1 - p_y_t)
     u = np.random.uniform(0, 1)
 
     return u <= acc_ratio
