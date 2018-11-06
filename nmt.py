800568118fe96d54588552ad3bb5a2f7dee9788d
Pengcheng Yin <pcyin@cs.cmu.edu>
2018-11-06 14:12:58 -0500

    removed raml from master

--- nmt.py
+++ nmt.py
@@ -5,7 +5,6 @@ A very basic implementation of neural machine translation
 
 Usage:
     nmt.py train --train-src=<file> --train-tgt=<file> --dev-src=<file> --dev-tgt=<file> --vocab=<file> [options]
-    nmt.py train-mcmc-raml --proposal-model=<file> --train-src=<file> --train-tgt=<file> --dev-src=<file> --dev-tgt=<file> --vocab=<file> [options]
     nmt.py decode [options] MODEL_PATH TEST_SOURCE_FILE OUTPUT_FILE
     nmt.py decode [options] MODEL_PATH TEST_SOURCE_FILE TEST_TARGET_FILE OUTPUT_FILE
 
@@ -17,7 +16,6 @@ Options:
     --dev-src=<file>                        dev source file
     --dev-tgt=<file>                        dev target file
     --vocab=<file>                          vocab file
-    --proposal-model=<file>                 proposal model in MCMC-RAML
     --seed=<int>                            seed [default: 0]
     --batch-size=<int>                      batch size [default: 32]
     --embed-size=<int>                      embedding size [default: 256]
@@ -58,7 +56,6 @@ import torch.nn.utils
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
-import raml_utils
 from utils import read_corpus, batch_iter, LabelSmoothingLoss
 from vocab import Vocab, VocabEntry
 
@@ -701,198 +698,6 @@ def train(args: Dict):
                     exit(0)
 
 
-def train_mcmc_raml(args: Dict):
-    train_data_src = read_corpus(args['--train-src'], source='src')
-    train_data_tgt = read_corpus(args['--train-tgt'], source='tgt')
-
-    dev_data_src = read_corpus(args['--dev-src'], source='src')
-    dev_data_tgt = read_corpus(args['--dev-tgt'], source='tgt')
-
-    train_data = list(zip(train_data_src, train_data_tgt))
-    dev_data = list(zip(dev_data_src, dev_data_tgt))
-
-    train_batch_size = int(args['--batch-size'])
-    clip_grad = float(args['--clip-grad'])
-    valid_niter = int(args['--valid-niter'])
-    log_every = int(args['--log-every'])
-    model_save_path = args['--save-to']
-    sample_size = int(args['--sample-size'])
-
-    vocab = pickle.load(open(args['--vocab'], 'rb'))
-
-    print(f"load proposal model from {args['--proposal-model']}", file=sys.stderr)
-    proposal_model = NMT.load(args['--proposal-model'])
-    proposal_model.eval()
-
-    model = NMT(embed_size=int(args['--embed-size']),
-                hidden_size=int(args['--hidden-size']),
-                dropout_rate=float(args['--dropout']),
-                input_feed=args['--input-feed'],
-                vocab=vocab)
-    model.train()
-
-    uniform_init = float(args['--uniform-init'])
-    if np.abs(uniform_init) > 0.:
-        print('uniformly initialize parameters [-%f, +%f]' % (uniform_init, uniform_init), file=sys.stderr)
-        for p in model.parameters():
-            p.data.uniform_(-uniform_init, uniform_init)
-
-    vocab_mask = torch.ones(len(vocab.tgt))
-    vocab_mask[vocab.tgt['<pad>']] = 0
-
-    device = torch.device("cuda:0" if args['--cuda'] else "cpu")
-    print('use device: %s' % device, file=sys.stderr)
-
-    model = model.to(device)
-    proposal_model = proposal_model.to(device)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=float(args['--lr']))
-
-    num_trial = 0
-    train_iter = patience = cum_loss = report_loss = cum_tgt_words = report_tgt_words = 0
-    cum_examples = report_examples = epoch = valid_num = 0
-    hist_valid_scores = []
-    train_time = begin_time = time.time()
-    print('begin MCMC RAML training')
-
-    while True:
-        epoch += 1
-
-        for src_sents, tgt_sents in batch_iter(train_data, batch_size=train_batch_size, shuffle=True):
-            train_iter += 1
-
-            optimizer.zero_grad()
-
-            with torch.no_grad():
-                p_gold_ys = proposal_model(src_sents, tgt_sents).numpy()
-
-                # generate samples
-                samples = proposal_model.sample(src_sents, sample_size=sample_size)
-
-            valid_samples = []
-            for src_sent_id, src_sent in enumerate(src_sents):
-                tgt_sent = tgt_sents[src_sent_id][1:-1]
-                p_gold_y = p_gold_ys[src_sent_id]
-                for sample_id in range(sample_size):
-                    sample = samples[src_sent_id][sample_id]
-                    r_i = raml_utils.get_reward(tgt_sent, sample.value)
-
-                    if raml_utils.mcmc_accept(p_gold_y, 1., sample.score, r_i, tau=1.):
-                    # if True:
-                        valid_samples.append((src_sent, sample.value))
-
-            total_sample_num = len(valid_samples)
-            print(f'Num. samples={len(total_sample_num)}', file=sys.stderr)
-            if total_sample_num == 0:
-                continue
-
-            retained_src_sents, retained_tgt_sents = zip(*valid_samples)
-
-            # (batch_size)
-            example_losses = -model(retained_src_sents, retained_tgt_sents)
-            batch_loss = example_losses.sum()
-            loss = batch_loss / total_sample_num
-
-            loss.backward()
-
-            # clip gradient
-            grad_norm = torch.nn.utils.clip_grad_norm(model.parameters(), clip_grad)
-
-            optimizer.step()
-
-            batch_losses_val = batch_loss.item()
-            report_loss += batch_losses_val
-            cum_loss += batch_losses_val
-
-            del loss, batch_loss
-
-            tgt_words_num_to_predict = sum(len(s[1:]) for s in retained_tgt_sents)  # omitting leading `<s>`
-            report_tgt_words += tgt_words_num_to_predict
-            cum_tgt_words += tgt_words_num_to_predict
-            report_examples += total_sample_num
-            cum_examples += total_sample_num
-
-            if train_iter % log_every == 0:
-                print('epoch %d, iter %d, avg. loss %.2f, avg. ppl %.2f ' \
-                      'cum. examples %d, speed %.2f words/sec, time elapsed %.2f sec' % (epoch, train_iter,
-                                                                                         report_loss / report_examples,
-                                                                                         math.exp(report_loss / report_tgt_words),
-                                                                                         cum_examples,
-                                                                                         report_tgt_words / (time.time() - train_time),
-                                                                                         time.time() - begin_time), file=sys.stderr)
-
-                train_time = time.time()
-                report_loss = report_tgt_words = report_examples = 0.
-
-            # the following code performs validation on dev set, and controls the learning schedule
-            # if the dev score is better than the last check point, then the current model is saved.
-            # otherwise, we allow for that performance degeneration for up to `--patience` times;
-            # if the dev score does not increase after `--patience` iterations, we reload the previously
-            # saved best model (and the state of the optimizer), halve the learning rate and continue
-            # training. This repeats for up to `--max-num-trial` times.
-
-            if train_iter % valid_niter == 0:
-                print('epoch %d, iter %d, cum. loss %.2f, cum. ppl %.2f cum. examples %d' % (epoch, train_iter,
-                                                                                         cum_loss / cum_examples,
-                                                                                         np.exp(cum_loss / cum_tgt_words),
-                                                                                         cum_examples), file=sys.stderr)
-
-                cum_loss = cum_examples = cum_tgt_words = 0.
-                valid_num += 1
-
-                print('begin validation ...', file=sys.stderr)
-
-                # compute dev. ppl and bleu
-                dev_ppl = evaluate_ppl(model, dev_data, batch_size=128)   # dev batch size can be a bit larger
-                valid_metric = -dev_ppl
-
-                print('validation: iter %d, dev. ppl %f' % (train_iter, dev_ppl), file=sys.stderr)
-
-                is_better = len(hist_valid_scores) == 0 or valid_metric > max(hist_valid_scores)
-                hist_valid_scores.append(valid_metric)
-
-                if is_better:
-                    patience = 0
-                    print('save currently the best model to [%s]' % model_save_path, file=sys.stderr)
-                    model.save(model_save_path)
-
-                    # also save the optimizers' state
-                    torch.save(optimizer.state_dict(), model_save_path + '.optim')
-                elif patience < int(args['--patience']):
-                    patience += 1
-                    print('hit patience %d' % patience, file=sys.stderr)
-
-                    if patience == int(args['--patience']):
-                        num_trial += 1
-                        print('hit #%d trial' % num_trial, file=sys.stderr)
-                        if num_trial == int(args['--max-num-trial']):
-                            print('early stop!', file=sys.stderr)
-                            exit(0)
-
-                        # decay learning rate, and restore from previously best checkpoint
-                        lr = optimizer.param_groups[0]['lr'] * float(args['--lr-decay'])
-                        print('load previously best model and decay learning rate to %f' % lr, file=sys.stderr)
-
-                        # load model
-                        params = torch.load(model_save_path, map_location=lambda storage, loc: storage)
-                        model.load_state_dict(params['state_dict'])
-                        model = model.to(device)
-
-                        print('restore parameters of the optimizers', file=sys.stderr)
-                        optimizer.load_state_dict(torch.load(model_save_path + '.optim'))
-
-                        # set new lr
-                        for param_group in optimizer.param_groups:
-                            param_group['lr'] = lr
-
-                        # reset patience
-                        patience = 0
-
-                if epoch == int(args['--max-epoch']):
-                    print('reached maximum number of epochs!', file=sys.stderr)
-                    exit(0)
-
-
 def beam_search(model: NMT, test_data_src: List[List[str]], beam_size: int, max_decoding_time_step: int) -> List[List[Hypothesis]]:
     was_training = model.training
     model.eval()
@@ -958,8 +763,6 @@ def main():
         train(args)
     elif args['decode']:
         decode(args)
-    elif args['train-mcmc-raml']:
-        train_mcmc_raml(args)
     else:
         raise RuntimeError(f'invalid run mode')
 
